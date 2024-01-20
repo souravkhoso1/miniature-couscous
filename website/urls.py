062bd7a39ff8da31385243f09d204ac967a713e7
Sourav Khoso <souravkhoso1@gmail.com>
2024-01-20 03:28:00 -0700

    added sitemap.xml and robots.txt

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {"static": StaticViewSitemap}
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap")  ,
     path("login/", views.login),
     path("auth/", views.auth, name="auth"),
     path("logout/", views.logout),
